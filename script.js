7359bd0f05a6e58cee57e21a0561cc24b427e118
Rayan Obeid <rayan.obeid93@gmail.com>
2023-10-07 12:47:46 +0300

    edit card size

--- script.js
+++ script.js
@@ -1,103 +1,105 @@
 
 
-
-function hide(){
-  let navHeader =document.querySelector('.ro-nav-head');
-  navHeader.innerHTML=``;
+//hide div above nav
+function hide() {
+  let navHeader = document.querySelector('.ro-nav-head');
+  navHeader.innerHTML = ``;
 }
 
-  let closeMark = function (icon){
-    let close=document.querySelector('.ro-close');
-    if(close.classList.contains("fa-bars")){
-    
+
+// convert burger menu button from bar to X 
+let closeMark = function (icon) {
+  let close = document.querySelector('.ro-close');
+  if (close.classList.contains("fa-bars")) {
+
     close.innerHTML = ` <i class="fa-solid fa-times navbar-toggler ro-close" style="color: #ff0000;"></i>`;
-    }
-
-    else if(close.classList.contains("fa-times")){
-      close.classList.replace("fa-bars" , "fa-times")
-    
-      }
-    
   }
-   const scrollTop =document.querySelector('.toTop');
-   scrollTop.addEventListener("click" , () =>{
-    window.scrollTo({
-      top:0,
-      behavior:"smooth",
 
-    });
-   });
+  else if (close.classList.contains("fa-times")) {
+    close.classList.replace("fa-bars", "fa-times")
+
+  }
+}
 
 
-   const result = fetch("https://dummyjson.com/products")
-   
+//fixed button to top
+const scrollTop = document.querySelector('.toTop');
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+
+  });
+});
+
+
+
+// new arrival section
+
+// pring data from fetch
+const result = fetch("https://dummyjson.com/products")
+
   .then(function (result) {
     return result.json();
-  }) 
+  })
   .then(function (result) {
     displayData(result.products);
 
   });
 
+function displayData(arr) {
+  let num;
+  for (let i = 1; i < 7; i++) {
+    let cardsArea = document.querySelector(".cards-area");
 
-  
-
-  function displayData(arr){
-    let num;
-    for(let i =1;i<7;i++){
-      let cardsArea =document.querySelector(".cards-area");
-
-      num=`<div class="card swiper-slide">
+    num = `<div class="card swiper-slide">
       <img src= ${arr[i].images[0]} class="card-img-top ro-swiper-img img-fluid" alt="...">
       <div class="card-body ro-card-body text-center">
         <h5 class="card-title  ">${arr[i].title}</h5>
-        <p class="card-text">Rating : ${Math.ceil(Number((arr[i].rating) *20))} %</p>
+        <p class="card-text">Rating : ${Math.ceil(Number((arr[i].rating) * 20))} %</p>
         <p class="text-center">Price : $ ${arr[i].price}</p>
       </div>
     </div>`
-     cardsArea.innerHTML+=num;
-     console.log(arr[i].images[0]);
-    }
-    }
-    
-  
+    cardsArea.innerHTML += num;
+    console.log(arr[i].images[0]);
+  }
+}
+
+//to cards slider
 var swiper = new Swiper(".slide-content", {
-                slidesPerView: 3,
-                spaceBetween: 30,
-                freeMode: true,
-                pagination: {
-                  el: ".swiper-pagination",
-                  clickable: true,
-                },
-                navigation: {
-                  nextEl: ".swiper-button-next",
-                  prevEl: ".swiper-button-prev",
-                },
-                breakpoints:{
-                  0:{
-                    slidesPerView: 1,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
 
-                  },
-                  700:{
-                    slidesPerView: 2,
+    },
+    700:{
+      slidesPerView: 2,
 
-                  },
-                  925:{
-                    slidesPerView: 3,
+    },
+    925:{
+      slidesPerView: 3,
 
-                  }
-
-
-                }
-              });
+    }
 
 
-  
-
-  
+  }
+});
 
 
 
-  
 
-  
+
+
+
+
